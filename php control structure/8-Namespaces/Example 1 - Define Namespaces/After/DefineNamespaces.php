cef23552242305de46e793f226fbbd129eecac33
testg2 <kkishanppanchal@gmail.com>
2020-12-18 11:40:49 +0530

    update notes

--- php control structure/8-Namespaces/Example 1 - Define Namespaces/After/DefineNamespaces.php
+++ php control structure/8-Namespaces/Example 1 - Define Namespaces/After/DefineNamespaces.php
@@ -4,8 +4,8 @@
      * 1) Namespaces are case-insensitive
      * 2) Namespaces are used to avoid name collision between other libraries.
      * 3) namespace is the keyword used to define namespace.
-     * 4) Only Classes, Interface, Functions and Constants should be defined in namepaces.
-     * 5) namespace should be always in the first line.
+     * 4) namespace should be always in the first line.
+     * 5) Only Classes, Interface, Functions and Constants should be defined in namepaces.
      * 6) include is used to include any file into another file.
      * 7) Use the backslash to refer to sub directories or file name.
      *
@@ -13,6 +13,8 @@
 
     //Include the namespace file
     include "NamespaceConstants.php";
+    include "namespacefun.php";
 
     //Access the Namespace.
     echo MyConstants\FILE_NAME;
+    echo namespacefun\CURRENT_FILENAME;
